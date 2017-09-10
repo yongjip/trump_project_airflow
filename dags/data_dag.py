eb6418335e4222389d779e955474573202b94260
COMSCORE_OFFICE\ykim <ykim@comscore.com>
2017-09-10 00:53:46 -0400

    Changed config. Added a dag updating db

---
+++ dags/data_dag.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'yongjip',
+    'depends_on_past': False,
+    'start_date': datetime(2017, 9, 7),
+    'email': ['yongjip@sas.upenn.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    'update_database', default_args=default_args, schedule_interval=timedelta(minutes=5))
+
+t1 = BashOperator(
+    task_id='update_database',
+    bash_command='python data/update_db.py',
+    dag=dag)
